fc35bcb0da4e7fc48e9411f12bcb7fb7f7cb7407
Torio <jpstorio@mymail.mapua.edu.ph>
2022-06-06 23:42:37 +0800

    project 1

--- src/components/DigitalBusinessCard/Footer.jsx
+++ src/components/DigitalBusinessCard/Footer.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import s from "../css/DigitalCard.module.css";
+import { FaTwitterSquare } from "react-icons/fa";
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaInstagramSquare } from "react-icons/fa";
+import { FaGithubSquare } from "react-icons/fa";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className={s.footer__container}>
+      <FaTwitterSquare className={s.icons__footer} />
+      <FaFacebookSquare className={s.icons__footer} />
+      <FaInstagramSquare className={s.icons__footer} />
+      <FaGithubSquare className={s.icons__footer} />
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/DigitalBusinessCard/Interests.jsx
+++ src/components/DigitalBusinessCard/Interests.jsx
@@ -5,17 +5,15 @@ function Interests() {
     <div className={s.interests__container}>
       <div className={s.about__heading}>About</div>
       <div className={s.content}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita sunt
-        reiciendis quos ea modi! Labore magni optio corrupti odio deleniti
-        veritatis, beatae numquam. Neque illo fugit enim sunt ab iste? Sit 
+        I am a frontend developer with a particular interest in making things
+        simple and automating daily tasks. I try to keep up with security and
+        best practices, and am always looking for new things to learn.
       </div>
 
       <div className={s.interests__heading}>Interests</div>
-      <div  className={s.content}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita sunt
-        reiciendis quos ea modi! Labore magni optio corrupti odio deleniti
-        veritatis, beatae numquam. Neque illo fugit enim sunt ab iste? Sit
-        temporibus assumenda, velit eaque sapiente vitae suscipit quis. Non 
+      <div className={s.content}>
+        Food expert. Music scholar. Reader. Internet fanatic. Bacon buff.
+        Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
       </div>
     </div>
   );
