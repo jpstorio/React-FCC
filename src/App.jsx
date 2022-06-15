753f4f9914110eda997ffdee5e1f228b2b7a1c47
Torio <jpstorio@mymail.mapua.edu.ph>
2022-06-15 21:32:24 +0800

    comment out

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import styles from './components/css/App.module.css';
-import FactSite from "./pages/FactSite";
+import styles from "./components/css/App.module.css";
+//import FactSite from "./pages/FactSite";
 //import Test from "./pages/Test";
-import DigitalCard from "./pages/DigitalCard";
+//import DigitalCard from "./pages/DigitalCard";
 
 function App() {
   return (
     <div className={styles.App}>
       {/* <Test /> */}
-      <FactSite />
-      <DigitalCard />
+      {/* <FactSite /> */}
+      {/* <DigitalCard /> */}
     </div>
   );
 }
